d2788d129e8e0769c3e573db6e402e6f10bc09df
Sharan <sharankasandula@gmail.com>
2021-05-22 16:52:36 +0530

    updated cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,51 @@
-const functions = require('firebase-functions');
-const puppeteer = require('puppeteer');
+const cors = require("cors")
+const puppeteer = require("puppeteer")
+const functions = require("firebase-functions");
 
-exports.getpdf = functions.https.onRequest(async (req, res) => {
-
-
-    var browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-
-    try {
-
-        const page = await browser.newPage();
-        await page.setContent("<html><head></head><body><h1>HELLO WORLD!</h1></body></html>")
-
-
-        const buffer = await page.pdf({
-            format: 'A4',
-            printBackground: true,
-            margin: {
-                left: '0px',
-                top: '0px',
-                right: '0px',
-                bottom: '0px'
-            }
-        })
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 
-        res.header('Content-Type', 'application/pdf;charset=utf-8');
-        res.header('Content-Disposition', 'attachment; filename=some_file.pdf');
-        res.type('application/pdf').send(buffer);
+const scrapeThingiverse = async (link) => {
+const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(link);
+const data = {};
+await page.content()
 
+data['author'] = await page.$eval("head > meta[name='author']", element => element.content);
+data['title'] = await page.$eval("head > meta[property='og:title']", element => element.content);
+data['description'] = await page.$eval("head > meta[property='og:description']", element => element.content);
+data['favicon'] = await page.$eval("head > link[rel='icon']", element => element.href);
 
-
-
-    } catch (e) {
-        res.status(500).send(e.toString());
+data['images'] = await page.evaluate(() => {
+    const imgUrls = document.querySelectorAll('li')
+    const urls = [];
+    for (let i = 0; i < imgUrls.length; i++) {
+        if (li.className === 'slide') {
+            urls.push(imgUrls[i].children[0].src)
+        } else {
+            urls.push('you messed up')
+        }
     }
+    console.log(JSON.stringify(urls))
+return urls;
+});
+await browser.close();
+return data;
+}
 
-       await browser.close();
 
+exports.scraper = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const body = JSON.parse(req.body);
+    const data = 'await scrapeThingiverse(body.text)';
+
+    response.send(data);
+  });
 });
